9b0407bc3f5f9ca2346e951e499244404a8ff42f
c4n1sl4v <cmihaylov@gmail.com>
2024-09-06 21:12:28 -0400

    modified:   app/components/leftsidebar.tsx

--- app/components/leftsidebar.tsx
+++ app/components/leftsidebar.tsx
@@ -1,6 +1,6 @@
 export default function LeftSidebar(){
     return (
-          <div className="hidden grid h-full place-content-center flex-[0.3] lg:block">
+          <div className="hidden h-full place-content-center flex-[0.3] lg:block">
       
      </div>
     )
